3e89cb7160dc6e19fad6378017109a2a0b66d806
AliaksandrHus <art-sonik@mail.ru>
2024-03-05 16:36:32 +0300

    language

--- BADBOT/avatars/models.py
+++ BADBOT/avatars/models.py
@@ -214,6 +214,8 @@ class Collection(models.Model):
     image720 = models.ImageField(upload_to='elements/name/')
 
 
+# Модель контента
+
 class Content(models.Model):
     """Модель контента"""
 
@@ -221,8 +223,11 @@ class Content(models.Model):
 
     # контент index
 
-    index_text_1 = models.TextField('Текст Index №1', blank=True)
-    index_text_2 = models.TextField('Текст Index №2', blank=True)
+    index_text_1_ru = models.TextField('Текст Index RU №1', blank=True)
+    index_text_2_ru = models.TextField('Текст Index RU №2', blank=True)
+
+    index_text_1_en = models.TextField('Текст Index EN №1', blank=True)
+    index_text_2_en = models.TextField('Текст Index EN №2', blank=True)
 
     index_img_1 = models.ImageField('Картинка Index №1', upload_to='media/design/content/', blank=True)
     index_img_2 = models.ImageField('Картинка Index №2', upload_to='media/design/content/', blank=True)
@@ -232,8 +237,11 @@ class Content(models.Model):
 
     # контент create
 
-    create_text_1 = models.TextField('Контент Create №1', blank=True)
-    create_text_2 = models.TextField('Контент Create №2', blank=True)
+    create_text_1_ru = models.TextField('Контент Create RU №1', blank=True)
+    create_text_2_ru = models.TextField('Контент Create RU №2', blank=True)
+
+    create_text_1_en = models.TextField('Контент Create EN №1', blank=True)
+    create_text_2_en = models.TextField('Контент Create EN №2', blank=True)
 
     create_img_1 = models.ImageField('Картинка Create №1', upload_to='media/design/content/', blank=True)
     create_img_2 = models.ImageField('Картинка Create №2', upload_to='media/design/content/', blank=True)
@@ -243,8 +251,11 @@ class Content(models.Model):
 
     # контент collection
 
-    collection_text_1 = models.TextField('Контент Collection №1', blank=True)
-    collection_text_2 = models.TextField('Контент Collection №2', blank=True)
+    collection_text_1_ru = models.TextField('Контент Collection RU №1', blank=True)
+    collection_text_2_ru = models.TextField('Контент Collection RU №2', blank=True)
+
+    collection_text_1_en = models.TextField('Контент Collection EN №1', blank=True)
+    collection_text_2_en = models.TextField('Контент Collection EN №2', blank=True)
 
     collection_img_1 = models.ImageField('Картинка Collection №1', upload_to='media/design/content/', blank=True)
     collection_img_2 = models.ImageField('Картинка Collection №2', upload_to='media/design/content/', blank=True)
@@ -254,6 +265,115 @@ class Content(models.Model):
 
     # объявление
 
-    index_message = models.TextField('Объявление Index', blank=True)
-    create_message = models.TextField('Объявление Create', blank=True)
-    collection_message = models.TextField('Объявление Collection', blank=True)
+    index_message_ru = models.TextField('Объявление Index RU', blank=True)
+    index_message_en = models.TextField('Объявление Index EN', blank=True)
+
+    create_message_ru = models.TextField('Объявление Create RU', blank=True)
+    create_message_en = models.TextField('Объявление Create EN', blank=True)
+
+    collection_message_ru = models.TextField('Объявление Collection RU', blank=True)
+    collection_message_en = models.TextField('Объявление Collection en', blank=True)
+
+
+# Модель футера
+
+class Footer(models.Model):
+    """Модель футера"""
+
+    id = models.IntegerField(primary_key=True)
+
+    # блок №1 ru
+
+    footer_box_1_name_ru = models.TextField('Заголовок блока #1 RU', blank=True, max_length=50)
+
+    footer_link_1_1_ru = models.TextField('Ссылка 1 блока #1 RU', blank=True)
+    footer_link_1_2_ru = models.TextField('Ссылка 2 блока #1 RU', blank=True)
+    footer_link_1_3_ru = models.TextField('Ссылка 3 блока #1 RU', blank=True)
+    footer_link_1_4_ru = models.TextField('Ссылка 4 блока #1 RU', blank=True)
+    footer_link_1_5_ru = models.TextField('Ссылка 5 блока #1 RU', blank=True)
+    footer_link_1_6_ru = models.TextField('Ссылка 6 блока #1 RU', blank=True)
+
+    # en
+
+    footer_box_1_name_en = models.TextField('Заголовок блока #1 EN', blank=True, max_length=50)
+
+    footer_link_1_1_en = models.TextField('Ссылка 1 блока #1 EN', blank=True)
+    footer_link_1_2_en = models.TextField('Ссылка 2 блока #1 EN', blank=True)
+    footer_link_1_3_en = models.TextField('Ссылка 3 блока #1 EN', blank=True)
+    footer_link_1_4_en = models.TextField('Ссылка 4 блока #1 EN', blank=True)
+    footer_link_1_5_en = models.TextField('Ссылка 5 блока #1 EN', blank=True)
+    footer_link_1_6_en = models.TextField('Ссылка 6 блока #1 EN', blank=True)
+
+    # блок №2 ru
+
+    footer_box_2_name_ru = models.TextField('Заголовок блока #2 RU', blank=True, max_length=50)
+
+    footer_link_2_1_ru = models.TextField('Ссылка 1 блока #2 RU', blank=True)
+    footer_link_2_2_ru = models.TextField('Ссылка 2 блока #2 RU', blank=True)
+    footer_link_2_3_ru = models.TextField('Ссылка 3 блока #2 RU', blank=True)
+    footer_link_2_4_ru = models.TextField('Ссылка 4 блока #2 RU', blank=True)
+    footer_link_2_5_ru = models.TextField('Ссылка 5 блока #2 RU', blank=True)
+    footer_link_2_6_ru = models.TextField('Ссылка 6 блока #2 RU', blank=True)
+
+    # en
+
+    footer_box_2_name_en = models.TextField('Заголовок блока #2 EN', blank=True, max_length=50)
+
+    footer_link_2_1_en = models.TextField('Ссылка 1 блока #2 EN', blank=True)
+    footer_link_2_2_en = models.TextField('Ссылка 2 блока #2 EN', blank=True)
+    footer_link_2_3_en = models.TextField('Ссылка 3 блока #2 EN', blank=True)
+    footer_link_2_4_en = models.TextField('Ссылка 4 блока #2 EN', blank=True)
+    footer_link_2_5_en = models.TextField('Ссылка 5 блока #2 EN', blank=True)
+    footer_link_2_6_en = models.TextField('Ссылка 6 блока #2 EN', blank=True)
+
+    # блок №3 ru
+
+    footer_box_3_name_ru = models.TextField('Заголовок блока #3 RU', blank=True, max_length=50)
+
+    footer_link_3_1_ru = models.TextField('Ссылка 1 блока #3 RU', blank=True)
+    footer_link_3_2_ru = models.TextField('Ссылка 2 блока #3 RU', blank=True)
+    footer_link_3_3_ru = models.TextField('Ссылка 3 блока #3 RU', blank=True)
+    footer_link_3_4_ru = models.TextField('Ссылка 4 блока #3 RU', blank=True)
+    footer_link_3_5_ru = models.TextField('Ссылка 5 блока #3 RU', blank=True)
+    footer_link_3_6_ru = models.TextField('Ссылка 6 блока #3 RU', blank=True)
+
+    # en
+
+    footer_box_3_name_en = models.TextField('Заголовок блока #3 EN', blank=True, max_length=50)
+
+    footer_link_3_1_en = models.TextField('Ссылка 1 блока #3 EN', blank=True)
+    footer_link_3_2_en = models.TextField('Ссылка 2 блока #3 EN', blank=True)
+    footer_link_3_3_en = models.TextField('Ссылка 3 блока #3 EN', blank=True)
+    footer_link_3_4_en = models.TextField('Ссылка 4 блока #3 EN', blank=True)
+    footer_link_3_5_en = models.TextField('Ссылка 5 блока #3 EN', blank=True)
+    footer_link_3_6_en = models.TextField('Ссылка 6 блока #3 EN', blank=True)
+
+    # строка с текстом ru
+
+    text_1_ru = models.TextField('Текст #1 RU', blank=True)
+    text_2_ru = models.TextField('Текст #2 RU', blank=True)
+
+    # en
+
+    text_1_en = models.TextField('Текст #1 EN', blank=True)
+    text_2_en = models.TextField('Текст #2 EN', blank=True)
+
+    # иконки ссылки
+
+    link_1 = models.TextField('Ссылка #1', blank=True)
+    icon_1 = models.ImageField('Иконка #1', upload_to='media/design/icons/', blank=True)
+
+    link_2 = models.TextField('Ссылка #2', blank=True)
+    icon_2 = models.ImageField('Иконка #2', upload_to='media/design/icons/', blank=True)
+
+    link_3 = models.TextField('Ссылка #3', blank=True)
+    icon_3 = models.ImageField('Иконка #3', upload_to='media/design/icons/', blank=True)
+
+    link_4 = models.TextField('Ссылка #4', blank=True)
+    icon_4 = models.ImageField('Иконка #4', upload_to='media/design/icons/', blank=True)
+
+    link_5 = models.TextField('Ссылка #5', blank=True)
+    icon_5 = models.ImageField('Иконка #5', upload_to='media/design/icons/', blank=True)
+
+    link_6 = models.TextField('Ссылка #6', blank=True)
+    icon_6 = models.ImageField('Иконка #6', upload_to='media/design/icons/', blank=True)
